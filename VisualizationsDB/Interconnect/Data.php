1e2eb319238afdbba72b0b817c0dbe1ec4d44a00
Matt Tooley <MTooley@NCTA.com>
2017-08-24 09:30:46 -0400

    g

--- VisualizationsDB/Interconnect/Data.php
+++ VisualizationsDB/Interconnect/Data.php
@@ -6,6 +6,6 @@ $date = $_GET["date"]; // Get date from GET request
 //$query = array("Date" => $date);
 //$document = $collection->findOne($query);
 $s=array('Date' => -1);
-$document = $collection->find()->sort($s)->limit(1) // Find the latest entry
+$document = $collection->find()->sort($s)->limit(1); // Find the latest entry
 echo json_encode($document);
 ?>
