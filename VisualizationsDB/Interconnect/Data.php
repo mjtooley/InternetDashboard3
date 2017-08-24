1820cc7655bbb4c128df2660bc1a72142384d38b
Matt Tooley <MTooley@NCTA.com>
2017-08-24 09:22:38 -0400

    g

--- VisualizationsDB/Interconnect/Data.php
+++ VisualizationsDB/Interconnect/Data.php
@@ -3,7 +3,8 @@ $connection = new MongoClient("mongodb://localhost:27017");
 $db = $connection->InternetDashboard;
 $collection = $db->interconnects;
 $date = $_GET["date"]; // Get date from GET request
-$query = array("Date" => $date);
-$document = $collection->findOne($query);
+//$query = array("Date" => $date);
+//$document = $collection->findOne($query);
+$document = $collection->find().sort({'Date':-1}).limit(1) // Find the latest entry
 echo json_encode($document);
 ?>
