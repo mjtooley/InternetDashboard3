952a9394ddfaf835485bbca2b0d22dc6c6fe6efa
Matt Tooley <MTooley@NCTA.com>
2017-08-24 09:54:39 -0400

    g

--- VisualizationsDB/Performance/Data.php
+++ VisualizationsDB/Performance/Data.php
@@ -3,7 +3,9 @@ $connection = new MongoClient("mongodb://localhost:27017");
 $db = $connection->InternetDashboard;
 $collection = $db->performance;
 $date = $_GET["date"]; // Get date from GET request
-$query = array("Date" => $date);
-$document = $collection->findOne($query);
+//$query = array("Date" => $date);
+//$document = $collection->findOne($query);
+$s=array('Date' => -1);
+$document = $collection->find()->sort($s)->limit(1); // Find the latest entry
 echo json_encode($document);
 ?>
